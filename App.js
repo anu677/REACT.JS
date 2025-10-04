1bbef6254f335defe06987a05980089258e52a18
anu677 <112054558+anu677@users.noreply.github.com>
2025-10-04 16:40:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/Header";
+
+function App() {
+  let myVariable=345;
+  return (
+    <>
+   <Header/>
+    </>
+    
+        
+  );
+}
+
+export default App;
